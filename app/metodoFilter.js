0620e0ba598994149849b36c119c4298d268a732
jojosuelobo <josue.lobo@etaure.com.br>
2023-05-22 16:44:06 -0300

    Topico 4

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,6 +5,7 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrar = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrar = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)  
     exibirOsLivrosNaTela(livrosFiltrar)
 }
+
