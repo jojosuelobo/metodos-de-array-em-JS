c790431f1288bca3a2f8710bfde495dfd0f7c8a4
jojosuelobo <josue.lobo@etaure.com.br>
2023-05-23 08:15:40 -0300

    Correção v2

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,9 +1,10 @@
-const botoes = document.querySelectorAll('.btn')
+const botoes = document.querySelectorAll('.btn');
+
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
-    exibirOsLivrosNaTela(livrosFiltrados)
+    let livrosFiltrar = categoria == 'disponivel' ? livros.filter(livros => livros.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)  
+    exibirOsLivrosNaTela(livrosFiltrar)
 }
